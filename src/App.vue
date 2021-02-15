dee96b51a248bb3a71b03b3b7a715eed416e65be
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2021-02-15 15:47:46 -0300

    p5

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
   <div id="app">
+    <div id="canvas"></div>
   </div>
 </template>
 
 <script>
+import P5 from "p5";
 
 export default {
   name: "App",
   components: {},
+  created() {
+    const sketch = (s) => {
+      let w = window.innerWidth;
+      let h = window.innerHeight;
+
+      s.setup = () => {
+        s.createCanvas(w, h);
+      };
+
+      s.draw = () => {
+        s.background(33, 33, 33);
+        s.ellipse(50, 50, 100, 100);
+        s.noLoop();
+
+        // call your method:
+        // s.yourMethod(x, y);
+      };
+
+      // create methods:
+      // s.yourMethod = (x, y) => {
+      //   // your method
+      // };
+    };
+
+    new P5(sketch, "canvas");
+  },
 };
 </script>
 
