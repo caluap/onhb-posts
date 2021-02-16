7ad21b16a01a5c7acb605101c1e9d2b6b6cc95dc
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2021-02-15 22:40:35 -0300

    resizes canvases

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,118 @@
 <template>
   <div id="app">
-    <div id="canvas"></div>
+    <div id="desktop">
+      <div id="canvas-container"></div>
+    </div>
+    <button @click="clickety()">Oi!</button>
+    <div id="toolbar">
+      <div>
+        <template v-for="(value, format) in formats">
+          <input
+            type="radio"
+            :id="`format-${format}`"
+            :key="`format-radio-${format}`"
+            :value="format"
+            v-model="socialMedia"
+            name="social_media_format"
+          />
+          <label :key="`format-label-${format}`" :for="`format-${format}`"
+            >{{ value }}
+          </label>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import P5 from "p5";
+import json from "@/assets/config.json";
 
 export default {
   name: "App",
+  data() {
+    return {
+      sketch: null,
+      p5instance: null,
+      canvas: null,
+      config: json,
+      socialMedia: "facebook_feed",
+      formats: {
+        facebook_feed: "Facebook",
+        twitter_feed: "Twitter",
+        instagram_feed: "Timeline Instagram",
+        instagram_stories: "Stories Instagram",
+      },
+      editions: ["ONHB13", "ONHB12", "Pré-2020", "Neutro"],
+    };
+  },
+  watch: {
+    socialMedia: function () {
+      this.p5instance.updateFormat();
+    },
+  },
   components: {},
-  created() {
-    const sketch = (s) => {
-      let w = window.innerWidth;
-      let h = window.innerHeight;
+  methods: {
+    clickety: function () {
+      this.p5instance.redraw();
+      this.p5instance.saveImg();
+    },
+  },
+  mounted() {
+    this.sketch = (s) => {
+      let mainFont, auxFont, auxFont2, logo;
+
+      s.preload = () => {
+        mainFont = s.loadFont("./fonts/CooperHewitt-Semibold.otf");
+        auxFont = s.loadFont("./fonts/CooperHewitt-Book.otf");
+        auxFont2 = s.loadFont("./fonts/CooperHewitt-MediumItalic.otf");
+        logo = s.loadImage("./img/logo.png");
+      };
 
       s.setup = () => {
-        s.createCanvas(w, h);
+        this.canvas = s.createCanvas(
+          this.config.formats[this.socialMedia].w,
+          this.config.formats[this.socialMedia].h
+        );
+        this.canvas.parent("canvas-container");
+        s.updateZoom();
+        console.log(`${mainFont}/${auxFont}/${auxFont2}/${logo}`);
+      };
+
+      s.updateZoom = () => {
+        // debugger; // eslint-disable-line no-debugger
+        let availableHSpace = document.getElementById("desktop").clientHeight,
+          ratioH = availableHSpace / this.config.formats[this.socialMedia].h,
+          availableWSpace = document.getElementById("desktop").clientWidth,
+          ratioW = availableWSpace / this.config.formats[this.socialMedia].w;
+
+        if (ratioW < 1 || ratioH < 1) {
+          let scale = Math.min(ratioW, ratioH);
+          this.canvas.elt.style.transform = `scale(${scale})`;
+        } else {
+          this.canvas.elt.style.transform = `scale(1)`;
+        }
+      };
+
+      s.updateFormat = () => {
+        let dimensions = this.config.formats[this.socialMedia];
+        s.resizeCanvas(dimensions.w, dimensions.h);
+        s.updateZoom();
+      };
+      s.updatePattern = () => {};
+      s.updateTextVars = () => {};
+
+      s.saveImg = () => {
+        let now = new Date();
+        let clock = now.getHours() + "·" + now.getMinutes();
+        let day = now.toJSON().slice(0, 10);
+        let fn = `post-${day}-${clock}.jpg`;
+        s.saveCanvas(this.canvas, fn, "jpg");
       };
 
       s.draw = () => {
-        s.background(33, 33, 33);
-        s.ellipse(50, 50, 100, 100);
+        s.background("#ff00ff");
         s.noLoop();
-
-        // call your method:
-        // s.yourMethod(x, y);
       };
 
       // create methods:
@@ -34,13 +121,46 @@ export default {
       // };
     };
 
-    new P5(sketch, "canvas");
+    this.p5instance = new P5(this.sketch, this.canvas);
   },
 };
 </script>
 
 <style lang="scss">
+$p: 3rem;
+
 body {
   margin: 0;
+}
+
+#toolbar {
+  position: fixed;
+  z-index: 1;
+
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: $p;
+  color: white;
+  padding: 0.25rem;
+
+  background: #333;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+#desktop {
+  // overflow-y: scroll;
+  position: fixed;
+  left: $p;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
